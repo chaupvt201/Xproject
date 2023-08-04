762f6500d225d825f4c2c4c4e77d553eb4a5344d
chaupvt201 <chauphamvt201@gmail.com>
2023-08-04 17:44:28 +0700

    commit

--- app/repositories/EmployeeRepo.php
+++ app/repositories/EmployeeRepo.php
@@ -7,22 +7,15 @@ class EmployeeRepo{
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         //2. Thực hiện truy vấn
         $sql = "SELECT * FROM employees ";
-        $stmt = $conn->prepare($sql); //Chuẩn bị thực hiện câu này
-        $stmt->execute(); //Thực thi đi >>> Nếu có kết quả trả về, nó sẽ lưu vào #stmt
-        //3. Xử lý kết quả (SELECT/INSERT-UPDATE-DELETE)
+        $stmt = $conn->prepare($sql); 
+        $stmt->execute();
+        
         $stmt->setFetchMode(PDO::FETCH_ASSOC);
-        //            $result = $stmt->fetch(); >> Chỉ lấy ra 1 bản ghi mỗi lần chạy
-        $result = $stmt->fetchAll(); //>> Lấy ra tất cả >>> Mảng các Mảng
-        //            Chuyển đổi Bản ghi (Mảng thông thường) sang Đối tượng: Post
+        $result = $stmt->fetchAll(); 
         $employees = []; 
         foreach($result as $row){
-//                echo $row['post_id'];
-//            $post = new Post($row['post_id'], $row['content'], $row['post_image'],$row['user_id'],$row['created_at'],'' );
             $employee = new Employee($row['id'], $row['name'], $row['address'], $row['salary']); 
-//                echo $post->getPostId();
-//                $posts[] = $post;
             $employees = $employee;
-//            array_push($posts, $post);
         }
         //4. Đóng kết nối
         $conn = null;
